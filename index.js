5bbc2481cfae72d06ccf479576aa87411164aeb9
pasly0920 <inwoo920621@gmail.com>
2021-12-03 23:51:24 +0900

    server start

---
+++ index.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const url = 'http://apis.data.go.kr/1471000/FoodNtrIrdntInfoService1/getFoodNtrItdntList1';
+let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'N41Inf4Co88iV2GFmLqoAZCKLjIcn1uV%2Frr3EJM%2F%2BX%2Br0URo3I1cCHat9hnBmeWn0MGmGlgemYyRGNA6STk8Jg%3D%3D'; /* Service Key*/
+queryParams += '&' + encodeURIComponent('desc_kor') + '=' + encodeURIComponent('새송이'); /* */
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* */
+queryParams += '&' + encodeURIComponent('type') + '=' + encodeURIComponent('json'); /* */
+
+request({
+    url: url + queryParams,
+    method: 'GET'
+}, function (error, response, _body) {
+    console.log('Status', response.statusCode);
+    console.log( _body);
+    console.log(_body.header);
+});
